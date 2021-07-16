cadb36b3bc199d5645f4fec390c7ce7af70c8711
Kandiak Oleg <kandio1604@gmail.com>
2021-07-16 14:00:41 +0300

    Updates

---
+++ precache-manifest.8c7dc3cd5f0312bfcdcf221f07753cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0fd0c9f4eb9f4af2eb15d13abc1f59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47726790c56741d144b5",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "986066df1e2614f0a9dc",
+    "url": "./static/css/main.d6a752ae.chunk.css"
+  },
+  {
+    "revision": "47726790c56741d144b5",
+    "url": "./static/js/2.cd70d490.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd70d490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "986066df1e2614f0a9dc",
+    "url": "./static/js/main.e0f47c60.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.8c7dc3cd5f0312bfcdcf221f07753cb9.js"
 );
 
 self.addEventListener('message', (event) => {
